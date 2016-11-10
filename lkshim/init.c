8653e701affc06ad4db594c5ac9448e7f0f3f5f7
Luca Weiss <luca@z3ntu.xyz>
2016-11-10 20:50:03 +0100

    Reformat code

--- lkshim/init.c
+++ lkshim/init.c
@@ -5,21 +5,21 @@
 uint32_t target_volume_down(void)
 {
     uint8_t status = 0;
-	struct pm8x41_gpio gpio;
+    struct pm8x41_gpio gpio;
 
-	/* Configure the GPIO */
-	gpio.direction = PM_GPIO_DIR_IN;
-	gpio.function  = 0;
-	gpio.pull      = PM_GPIO_PULL_UP_30;
-	gpio.vin_sel   = 2;
+    /* Configure the GPIO */
+    gpio.direction = PM_GPIO_DIR_IN;
+    gpio.function = 0;
+    gpio.pull = PM_GPIO_PULL_UP_30;
+    gpio.vin_sel = 2;
 
-	pm8x41_gpio_config(2, &gpio);
+    pm8x41_gpio_config(2, &gpio);
 
-	/* Wait for the pmic gpio config to take effect */
-	thread_sleep(1);
+    /* Wait for the pmic gpio config to take effect */
+    thread_sleep(1);
 
-	/* Get status of P_GPIO_2 */
-	pm8x41_gpio_get(2, &status);
+    /* Get status of P_GPIO_2 */
+    pm8x41_gpio_get(2, &status);
 
-	return !status; /* active low */
+    return !status; /* active low */
 }
