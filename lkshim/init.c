4f10086783ec62271d687d41d545a2a4b67beddf
M1cha <sigmaepsilon92@gmail.com>
2016-11-10 16:25:42 +0100

    lkshim: reformat

--- lkshim/init.c
+++ lkshim/init.c
@@ -5,27 +5,27 @@
 
 int target_volume_down(void)
 {
-	static uint8_t first_time = 0;
-	uint8_t status = 0;
-	struct pm8x41_gpio gpio;
+    static uint8_t first_time = 0;
+    uint8_t status = 0;
+    struct pm8x41_gpio gpio;
 
-	if (!first_time) {
-		/* Configure the GPIO */
-		gpio.direction = PM_GPIO_DIR_IN;
-		gpio.function  = 0;
-		gpio.pull      = PM_GPIO_PULL_UP_30;
-		gpio.vin_sel   = 2;
+    if (!first_time) {
+        /* Configure the GPIO */
+        gpio.direction = PM_GPIO_DIR_IN;
+        gpio.function  = 0;
+        gpio.pull      = PM_GPIO_PULL_UP_30;
+        gpio.vin_sel   = 2;
 
-		pm8x41_gpio_config(2, &gpio);
+        pm8x41_gpio_config(2, &gpio);
 
-		/* Wait for the pmic gpio config to take effect */
-		udelay(10000);
+        /* Wait for the pmic gpio config to take effect */
+        udelay(10000);
 
-		first_time = 1;
-	}
+        first_time = 1;
+    }
 
-	/* Get status of P_GPIO_5 */
-	pm8x41_gpio_get(2, &status);
+    /* Get status of P_GPIO_5 */
+    pm8x41_gpio_get(2, &status);
 
-	return !status; /* active low */
+    return !status; /* active low */
 }
