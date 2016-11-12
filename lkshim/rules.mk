1e272a1cfd3508f8c5d3d438ac68161ff9f0d606
Luca Weiss <z3ntu@users.noreply.github.com>
2016-11-12 11:05:55 +0100

    Make target_volume_down better and add the camera key as a back button (#4)

--- lkshim/rules.mk
+++ lkshim/rules.mk
@@ -1,8 +1,8 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
-#ifeq ($(WITH_KERNEL_UEFIAPI),1)
-#	SHIMOBJS += msm8974.o
-#endif
+ifeq ($(WITH_KERNEL_UEFIAPI),1)
+	SHIMOBJS += $(LOCAL_DIR)/msm8974.o
+endif
 
 SHIMOBJS += \
 	$(LOCAL_DIR)/init.o
