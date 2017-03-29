257f0254063d23d37546cfcb9d4af3efc603e887
sub77 <sub77@ymail.com>
2017-03-29 15:00:07 +0000

    update

--- lk_inc.mk
+++ lk_inc.mk
@@ -1,10 +1,3 @@
-EMMC_BOOT := 1
-DISPLAY_2NDSTAGE := 1
-
-# it's not nice to rely on the original values here but it's the best we can do right now
-DEFINES := $(filter-out ABOOT_FORCE_RAMDISK_ADDR=0x08800000,$(DEFINES))
-DEFINES := $(filter-out ABOOT_FORCE_TAGS_ADDR=0x09000000,$(DEFINES))
-
-DEFINES += ABOOT_FORCE_RAMDISK_ADDR=0x02000000
-DEFINES += ABOOT_FORCE_TAGS_ADDR=0x01e00000
-
+EMMC_BOOT                := 1
+DISPLAY_2NDSTAGE         := 1
+DEVICE_PREFER_ATAGS_HWID := 1
