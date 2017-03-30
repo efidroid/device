d012f4387d0aa1302f0b1201171c0f9d89d0b228
Hannu Markkula <hannu.markkula007@gmail.com>
2017-03-30 22:42:19 +0300

    Yotaphone

--- lkshim/keypad.c
+++
@@ -1,9 +0,0 @@
-#include <dev/pm8921.h>
-
-#define BITS_IN_ELEMENT(x) (sizeof(x) * 8)
-#define KEYMAP_INDEX(row, col) (row)* BITS_IN_ELEMENT(unsigned int) + (col)
-
-unsigned int apq8064_pm8921_keys_gpiomap[] = {
-	[KEYMAP_INDEX(0, 0)] = PM_GPIO(1),	/* Volume key on the device/CDP */
-	[KEYMAP_INDEX(0, 1)] = PM_GPIO(2),	/* Volume key on the device/CDP */
-};
